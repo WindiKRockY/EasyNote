61e0ffbb62225b08f38fe3208ef52b31425826bd
WindiKRockY <152081374+WindiKRockY@users.noreply.github.com>
2024-03-03 17:33:04 +0200

    Version 1.3
    +New functions

--- main.py
+++ main.py
@@ -15,17 +15,26 @@ class Widget(QMainWindow):
         self.ui.savenote_btn.clicked.connect(self.save_note)
         self.ui.createnote_btn.clicked.connect(self.create_note)
         self.ui.delatenote_btn.clicked.connect(self.delete_note)
+        self.ui.createteg_btn.clicked.connect(self.add_tag)
+        self.ui.deleteteg_btn.clicked.connect(self.delete_teg)
 
     def show_note(self):
         self.name = self.ui.list_wid.selectedItems()[0].text()
         self.ui.findnote_btn.setText(self.name)
         self.ui.note_wid.setText(self.notes[self.name]["Text"])
+        self.ui.teg_wid.clear()
+        self.ui.teg_wid.addItems(self.notes[self.name]["Tegs"])
 
 
     def save_note(self):
+        tags = []
+        for i in range(self.ui.teg_wid.count()):
+            tags.append(self.ui.teg_wid.item(i).text())
+
+
         self.notes[self.ui.findnote_btn.text()]=  {
             'Text':self.ui.note_wid.toPlainText(),
-            'Tegs':[]
+            'Tegs':tags
         }
         with open("notes.json","w",encoding="utf-8") as file:
             json.dump(self.notes, file)
@@ -58,7 +67,22 @@ class Widget(QMainWindow):
             self.save_note()
         except:
             print('Eror404')
-
+    def add_tag(self):
+        tag_name = self.ui.findteg_btn.text()
+        if tag_name !="":
+            if tag_name not in self.notes[self.name]["Tegs"]:
+                self.notes[self.name]["Tegs"].append(tag_name)
+                self.ui.teg_wid.clear()
+                self.ui.teg_wid.addItems(self.notes[self.name]["Tegs"])
+    
+    def delete_teg(self):
+        if self.ui.teg_wid.selectedItems():
+            tag_name = self.ui.teg_wid.selectedItems()[0].text()
+            if tag_name  in self.notes[self.name]["Tegs"]:
+                self.notes[self.name]["Tegs"].remove(tag_name)
+                self.ui.teg_wid.clear()
+                self.ui.teg_wid.addItems(self.notes[self.name]["Tegs"])
+        
 app = QApplication([])
 ex = Widget()
 ex.show()
